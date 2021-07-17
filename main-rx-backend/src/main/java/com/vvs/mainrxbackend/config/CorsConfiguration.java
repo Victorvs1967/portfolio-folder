4122881521667eb4ee31779497e7f1dd4a287faa
Victors1967 <victorsmirnov@me.com>
2021-07-17 15:37:14 +0300

    111

---
+++ main-rx-backend/src/main/java/com/vvs/mainrxbackend/config/CorsConfiguration.java
@@ -0,0 +1,9 @@
+package com.vvs.mainrxbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CorsConfiguration {
+  
+
+}
--- main-rx-backend/src/main/java/com/vvs/mainrxbackend/router/ToDoRouter.java
+++ main-rx-backend/src/main/java/com/vvs/mainrxbackend/router/ToDoRouter.java
@@ -15,10 +15,12 @@ public class ToDoRouter {
   @Bean
   public RouterFunction<ServerResponse> monoRouterFunction(ToDoHandler todoHandler) {
     
-    return RouterFunctions.route(GET("/todo/{id}").and(accept(MediaType.APPLICATION_JSON)), todoHandler::getToDo)
+    return RouterFunctions
+            .route(GET("/todo/{id}").and(accept(MediaType.APPLICATION_JSON)), todoHandler::getToDo)
             .andRoute(GET("/todo").and(accept(MediaType.APPLICATION_JSON)), todoHandler::getToDos)
             .andRoute(POST("/todo").and(accept(MediaType.APPLICATION_JSON)), todoHandler::newToDo)
             .andRoute(PUT("/todo").and(accept(MediaType.APPLICATION_JSON)), todoHandler::updateToDo)
             .andRoute(DELETE("/todo/{id}").and(accept(MediaType.APPLICATION_JSON)), todoHandler::deleteTodo);
   }
+
 }
