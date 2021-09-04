7b36d8b0819d2d73f14f23b76a03cdf3bbc080c1
Victors1967 <victorsmirnov@me.com>
2021-09-04 22:25:10 +0300

    0000

--- main-rx-frontend/src/environments/environment.prod.ts
+++ main-rx-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  authUrl: 'http://172.31.0.1:8080',
-  todoUrl: 'http://172.31.0.1:8080/todo'
+  authUrl: 'http://localhost:8080',
+  todoUrl: 'http://localhost:8080/todo'
 };
