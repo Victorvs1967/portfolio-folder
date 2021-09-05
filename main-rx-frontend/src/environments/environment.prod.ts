74cb2c18bf846c6d40586784a0d5b7545a5407c3
Victors1967 <victorsmirnov@me.com>
2021-09-05 21:10:48 +0300

    1111

--- main-rx-frontend/src/environments/environment.prod.ts
+++ main-rx-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  authUrl: 'http://185.161.208.235:8082',
-  todoUrl: 'http://185.161.208.235:8082/todo'
+  authUrl: 'http://185.161.208.235:8080',
+  todoUrl: 'http://185.161.208.235:8080/todo'
 };
