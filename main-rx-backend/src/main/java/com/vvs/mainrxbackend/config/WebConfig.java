d78109db919467d6316589a80f991a39699f5d5d
Victors1967 <victorsmirnov@me.com>
2021-09-06 21:46:18 +0300

    add variable

--- main-rx-backend/src/main/java/com/vvs/mainrxbackend/config/WebConfig.java
+++ main-rx-backend/src/main/java/com/vvs/mainrxbackend/config/WebConfig.java
@@ -1,17 +1,20 @@
 package com.vvs.mainrxbackend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
 public class WebConfig implements WebFluxConfigurer {
+
+  @Value("${app.server.host}")
+  private String host;
   
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-      .allowedOrigins("http://185.161.208.235:4200")
-      // .allowedOrigins("http://localhost:4200")
+      .allowedOrigins("http://" + host + ":4200") // for production
       .allowedMethods("*")
       .allowedHeaders("*")
       .allowCredentials(true)
