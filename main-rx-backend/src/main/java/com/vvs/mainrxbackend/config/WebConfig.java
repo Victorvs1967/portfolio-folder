205380f12360891ab361e710becc58a3edc79355
Victors1967 <victorsmirnov@me.com>
2021-09-06 16:03:17 +0300

    2222

--- main-rx-backend/src/main/java/com/vvs/mainrxbackend/config/WebConfig.java
+++ main-rx-backend/src/main/java/com/vvs/mainrxbackend/config/WebConfig.java
@@ -12,7 +12,8 @@ public class WebConfig implements WebFluxConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-      .allowedOrigins("http://localhost:4200")
+      .allowedOrigins("http://http://185.161.208.235:4200")
+      // .allowedOrigins("http://localhost:4200")
       .allowedMethods("*")
       .allowedHeaders("*")
       .allowCredentials(true)
