95aea8460d45adca986365b561ef21b2ec0d0718
Victors1967 <victorsmirnov@me.com>
2021-09-06 15:17:49 +0300

    2222

--- main-rx-frontend/src/environments/environment.prod.ts
+++ main-rx-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  authUrl: 'http://192.168.32.3:8080',
-  todoUrl: 'http://192.168.32.3:8080/todo'
+  authUrl: 'http://localhost:8080',
+  todoUrl: 'http://localhost:8080/todo'
 };
