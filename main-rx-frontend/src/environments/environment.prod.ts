20ee86083b1724dcfae32a52eb5491b4aa0fc4ab
Victors1967 <victorsmirnov@me.com>
2021-09-07 17:21:04 +0300

    www

--- main-rx-frontend/src/environments/environment.prod.ts
+++ main-rx-frontend/src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: true,
-  authUrl: 'http://v10549.dh.net.ua:8080',
-  todoUrl: 'http://v10549.dh.net.ua:8080/todo'
+  authUrl: 'http://www.portfolio-dev.club:8080',
+  todoUrl: 'http://www.portfolio-dev.club:8080/todo'
+  // authUrl: 'http://v10549.dh.net.ua:8080',
+  // todoUrl: 'http://v10549.dh.net.ua:8080/todo'
   // authUrl: 'http://185.161.208.235:8080',
   // todoUrl: 'http://185.161.208.235:8080/todo'
 };
