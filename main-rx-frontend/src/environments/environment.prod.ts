56c1f020b942144a94880161147d68ed83310c10
Victors1967 <victorsmirnov@me.com>
2021-09-07 14:46:14 +0300

    3333

--- main-rx-frontend/src/environments/environment.prod.ts
+++ main-rx-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  authUrl: 'http://185.161.208.235:8080',
-  todoUrl: 'http://185.161.208.235:8080/todo'
+  authUrl: 'http://v10549.dh.net.ua:8080',
+  todoUrl: 'http://v10549.dh.net.ua:8080/todo'
+  // authUrl: 'http://185.161.208.235:8080',
+  // todoUrl: 'http://185.161.208.235:8080/todo'
 };
