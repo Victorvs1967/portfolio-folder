2a3f5d995a41566c0621a2147de7a7f9dee89c1f
Victors1967 <victorsmirnov@me.com>
2021-09-08 13:08:58 +0300

    edit ip

--- main-rx-frontend/src/environments/environment.prod.ts
+++ main-rx-frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  authUrl: 'http://172.19.0.2:8080',
-  todoUrl: 'http://172.19.0.2:8080/todo'
+  authUrl: 'http://172.18.0.2:8080',
+  todoUrl: 'http://172.18.0.2:8080/todo'
   // authUrl: 'http://www.portfolio-dev.club:8080',
   // todoUrl: 'http://www.portfolio-dev.club:8080/todo'
 };
