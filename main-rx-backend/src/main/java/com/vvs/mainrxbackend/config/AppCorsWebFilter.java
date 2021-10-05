cc6f864732a33b506b1208328e5aeb22c647c182
Victors1967 <victorsmirnov@me.com>
2021-10-05 08:26:23 +0300

    3333

--- main-rx-backend/src/main/java/com/vvs/mainrxbackend/config/AppCorsWebFilter.java
+++ main-rx-backend/src/main/java/com/vvs/mainrxbackend/config/AppCorsWebFilter.java
@@ -19,7 +19,8 @@ public class AppCorsWebFilter {
     // Possibly...
     // config.applyPermitDefaultValues();
 
-    config.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:80", "http://localhost:8080"));
+    // config.setAllowedOrigins(Arrays.asList("http://185.161.208.235:4200", "http://localhost:4200", "http://localhost:80", "http://localhost:8080"));
+    config.setAllowedOrigins(Arrays.asList("http://185.161.208.235:4200", "http://185.161.208.235:80", "http://185.161.208.235:8080"));
     config.addAllowedHeader("*");
     config.addAllowedMethod("*");
     config.setAllowCredentials(true);
--- main-rx-frontend/src/environments/environment.prod.ts
+++ main-rx-frontend/src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
-  // authUrl: 'http://185.161.208.235:8080',
-  // todoUrl: 'http://185.161.208.235:8080/todo'
-  authUrl: 'http://localhost:8080',
-  todoUrl: 'http://localhost:8080/todo'
-
+  authUrl: 'http://185.161.208.235:8080',
+  todoUrl: 'http://185.161.208.235:8080/todo'
+  // authUrl: 'http://localhost:8080',
+  // todoUrl: 'http://localhost:8080/todo'
 };
